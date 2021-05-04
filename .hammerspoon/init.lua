8c0262c24c4d3898bd4bb5de11af8e5d6ca851e5
keke <librek@protonmail.com>
2021-05-04 17:16:16 +0800

    更改键位、kitty

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,29 +1,32 @@
-	function getWinList(name)
-    return hs.window.filter.new(false):setAppFilter(name, { currentSpace = true }):getWindows()
+function getWinList(name)
+   return hs.window.filter.new(false):setAppFilter(name, { currentSpace = true }):getWindows()
 end
+
 
 function launchOrNextWindow(name, showName)
-    local findName = showName or name
-    local appName = hs.application.frontmostApplication():name()
-    if findName ~= appName then
-        hs.application.launchOrFocus(name)
-    else
-        local wlist = getWinList(findName)
-        local wcount = #wlist
-        if wcount > 1 then
-            hs.eventtap.keyStroke({'cmd'}, '`')
-        else
-            local win = wlist[1]
-            if win:isMinimized() then win:unminimize() else win:minimize() end
-        end
-    end
+   local findName = showName or name
+   local appName = hs.application.frontmostApplication():name()
+   if findName ~= appName then
+      hs.application.launchOrFocus(name)
+   else
+      local wlist = getWinList(findName)
+      local wcount = #wlist
+      if wcount > 1 then
+	 hs.eventtap.keyStroke({'cmd'}, '`')
+      else
+	 local win = wlist[1]
+	 if win:isMinimized() then win:unminimize() else win:minimize() end
+      end
+   end
 end
 
+
 function mapLaunch(key, name, showName)
-    hs.hotkey.bind({"cmd", "alt"}, key, function()
-        launchOrNextWindow(name, showName)
-    end)
+   hs.hotkey.bind({'cmd','alt','shift','ctrl'}, key, function()
+	 launchOrNextWindow(name, showName)
+   end)
 end
+
 
 mapLaunch('s', 'Firefox')
 mapLaunch('e', 'Emacs')
@@ -34,63 +37,63 @@ mapLaunch('f', 'Finder')
 mapLaunch('a', 'Kitty')
 
 function moveLeftHalf()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   local screen = win:screen()
+   local max = screen:frame()
+   f.x = max.x
+   f.y = max.y
+   f.w = max.w / 2
+   f.h = max.h
+   win:setFrame(f)
 end
 
 function moveRightHalf()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   local screen = win:screen()
+   local max = screen:frame()
+   f.x = max.x + (max.w / 2)
+   f.y = max.y
+   f.w = max.w / 2
+   f.h = max.h
+   win:setFrame(f)
 end
 
 function maximize()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
+   local win = hs.window.focusedWindow()
+   local f = win:frame()
+   local screen = win:screen()
+   local max = screen:frame()
+   f.x = max.x
+   f.y = max.y
+   f.w = max.w
+   f.h = max.h
+   win:setFrame(f)
 end
 
 function moveToScreen(screenPos)
-  window = hs.window.focusedWindow()
-  screen = hs.screen.find({x=screenPos, y=0})
-  window:moveToScreen(screen)
+   window = hs.window.focusedWindow()
+   screen = hs.screen.find({x=screenPos, y=0})
+   window:moveToScreen(screen)
 end
-hs.hotkey.bind({'alt'}, "1", function()
-  moveToScreen(0)
-  moveLeftHalf()
+hs.hotkey.bind({'cmd','alt','shift','ctrl'}, "1", function()
+      moveToScreen(0)
+      moveLeftHalf()
 end)
 
-hs.hotkey.bind({'alt'}, "2", function()
-  moveToScreen(0)
-  moveRightHalf()
+hs.hotkey.bind({'cmd','alt','shift','ctrl'}, "2", function()
+      moveToScreen(0)
+      moveRightHalf()
 end)
 
-hs.hotkey.bind({'alt'}, "3", function()
-  moveToScreen(1)
-  moveLeftHalf()
+hs.hotkey.bind({'cmd','alt','shift','ctrl'}, "3", function()
+      moveToScreen(1)
+      moveLeftHalf()
 end)
 
-hs.hotkey.bind({'alt'}, "4", function()
-  moveToScreen(1)
-  moveRightHalf()
+hs.hotkey.bind({'cmd','alt','shift','ctrl'}, "4", function()
+      moveToScreen(1)
+      moveRightHalf()
 end)
-hs.hotkey.bind({'alt'}, "z", maximize)
+hs.hotkey.bind({'cmd','alt','shift','ctrl'}, "z", maximize)
--- .zprofile
+++ .zprofile
@@ -1,2 +1,1 @@
 alias pass="./Repos/password-store/src/password-store.sh"
-export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
